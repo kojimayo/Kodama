d3d487c52e8e954f875874b47a951f6ceb02e576
kojimayo <yoshiyuki1218@mx5.canvas.ne.jp>
2015-11-08 20:17:52 +0900

    Initial Commit

---
+++ KodamaTests/KodamaTests.swift
@@ -0,0 +1,36 @@
+//
+//  KodamaTests.swift
+//  KodamaTests
+//
+//  Created by 小島 佳幸 on 2015/11/08.
+//  Copyright (c) 2015年 小島 佳幸. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class KodamaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
